53993f52736a9ce440818c903cb2591c1360ad7a
Kaida Kohki <kkohki@c1r27s3.42tokyo.jp>
2023-02-21 23:33:23 +0900

    & を忘れていたので追加した

--- ex00/Warlock.hpp
+++ ex00/Warlock.hpp
@@ -17,8 +17,8 @@ class Warlock
 	public:
 		const std::string &getName() const;
 		const std::string &getTitle() const;
-		void setTitle(const std::string title);
-		Warlock(const std::string name,const std::string title);
+		void setTitle(const std::string &title);
+		Warlock(const std::string &name,const std::string &title);
 		~Warlock();
 		void introduce() const;
 };
